ed3b46ab70cb77d7f5edf9348a9e042fc9cc0dbf
LAPTOP-Q5UA3DQ7\Vighnesh Dandekar <vighnesh.dandekar@wabodryms.com>
2017-09-30 14:20:07 +0530

    8080 should be the port for reset apis

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 // server.js
-
-
 var express = require("express");
-var bodyParser = require("body-parser");
+// var bodyParser = require("body-parser");
+var bodyParser = require('body-parser').json();
+
 var stripe = require('stripe')("sk_test_o582mFkZFmS94mvRLtlhWcFx");
+// stripe.setTimeout(20000);
 
 var app = express();
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
-app.set('port', (process.env.PORT || 5000));
-
-
-
-
+app.set('port', (process.env.PORT || 8080));
 
 var server = app.listen(app.get('port'), function () {
     // var port = server.address().port;
-    console.log("App now running on port", app.get('port'));
+
+    console.log("App now running on port", new Date().toLocaleString());
 });
 
 
@@ -33,19 +36,29 @@ function handleError(res, reason, message, code) {
 }
 
 
+app.get('/customer', function (req, res) {
+    console.log('/customer');
+    res.send({ 'test': 'comepeplted' });
+
+
+})
+
 app.post('/customer', bodyParser, function (req, res) {
     console.log('customer', JSON.stringify(req.body))
-    stripe.customers.create({
-        email: req.body.email
-    }, function (err, success) {
-        if (err) {
-            res.send({ 'status': false, "err": err });
-        }
-        if (success) {
-            // res.status(201).json(doc.ops[0]);
-            res.send({ 'status': true, "success": success });
-        }
+    res.send({ 'test': 'comepeplted', 'param': req.body });
 
-        // asynchronously called
-    });
+    // stripe.customers.create({
+    //     email: req.body.email
+    // }, function (err, success) {
+    //     if (err) {
+    //         console.log('error', err)
+    //         res.send({ 'status': false, "err": err });
+    //     }
+    //     if (success) {
+    //         console.log('success')
+    //         // res.status(201).json(doc.ops[0]);
+    //         res.send({ 'status': true, "success": success });
+    //     }
+
+    // });
 });
